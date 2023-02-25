5083aac94dd67e50311d49d61d656ba723ccd11e
Eeshan Keni <19875557+eeshankeni@users.noreply.github.com>
2023-02-26 00:09:38 +0530

    init

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
+import { avataaars } from '@dicebear/collection';
+import { createAvatar } from '@dicebear/core';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import {SvgCss} from 'react-native-svg';
+
 
 export default function App() {
+
+  let avatar = createAvatar(avataaars, {
+    seed: 'John Doe',
+    style:['circle']
+    // ... other options
+  });
+  
+  const svg = avatar.toString();
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+                      <SvgCss xml={svg} width={300} height={300}/>
+
+     </View>
   );
 }
 
